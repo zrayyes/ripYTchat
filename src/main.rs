6e4794e181d883f5161dc2a87209b1594b2ebaa1
Zuhair Rayyes <rayyes95@gmail.com>
2022-10-14 22:46:02 +0200

    moved out of main fn

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,63 @@
-use lazy_static::lazy_static;
 use regex::Regex;
 
-fn get_continuation_key(text: &str) -> Option<regex::Match> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r#""continuation":"(.*?)","#).unwrap();
-    }
-    let caps = RE.captures(text).unwrap();
-    caps.get(1)
+#[derive(Debug)]
+struct Video {
+    id: String,
+    api_key: String,
+    first_continuation_key: String,
+    title: String,
+    channel_name: String,
+    channel_id: String,
 }
 
-fn get_api_key(text: &str) -> Option<regex::Match> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r#""INNERTUBE_API_KEY":"(.*?)","#).unwrap();
+impl Video {
+    async fn from_id(video_id: &str) -> Video {
+        let video_url = format!("https://www.youtube.com/watch?v={}", &video_id);
+
+        let body = reqwest::get(&video_url)
+            .await
+            .expect("Failed to fetch video page from video ID.")
+            .text()
+            .await
+            .expect("Failed to get video page text.");
+        let continuation_key = get_key_value_from_body("continuation", &body)
+            .unwrap()
+            .as_str();
+        let api_key = get_key_value_from_body("INNERTUBE_API_KEY", &body)
+            .unwrap()
+            .as_str();
+        let title = get_key_value_from_body("title", &body).unwrap().as_str();
+        let channel_name = get_key_value_from_body("author", &body).unwrap().as_str();
+        let channel_id = get_key_value_from_body("channelId", &body)
+            .unwrap()
+            .as_str();
+
+        Video {
+            id: video_id.to_string(),
+            api_key: api_key.to_string(),
+            first_continuation_key: continuation_key.to_string(),
+            title: title.to_string(),
+            channel_name: channel_name.to_string(),
+            channel_id: channel_id.to_string(),
+        }
     }
-    let caps = RE.captures(text).unwrap();
+}
+
+fn get_key_value_from_body<'a>(key: &'a str, text: &'a str) -> Option<regex::Match<'a>> {
+    let re_str = format!(r#""{}":"(.*?)","#, key);
+    let re = Regex::new(&re_str).unwrap();
+    let caps = re.captures(text).unwrap();
     caps.get(1)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: Read video id from args
     let video_id = "OLKhm1jKrVM";
-    let video_url = format!("https://www.youtube.com/watch?v={}", &video_id);
-
-    let client = reqwest::Client::new();
-    let body = client.get(&video_url).send().await?.text().await?;
-    let continuation_key = get_continuation_key(&body).unwrap().as_str();
-    let api_key = get_api_key(&body).unwrap().as_str();
-    println!("{}, {}", continuation_key, api_key);
+    let video = Video::from_id(video_id).await;
+    print!("{:?}", video);
+    //TODO: get_live_chat_replay
+    //TODO: parse body
+    //TODO: Write to file
     Ok(())
 }
