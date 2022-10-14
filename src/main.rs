f66dd4d9026942bbeb9749b73c7ae97a78679c9c
Zuhair Rayyes <rayyes95@gmail.com>
2022-10-14 22:51:17 +0200

    read from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{env, process::exit};
+
 use regex::Regex;
 
 #[derive(Debug)]
@@ -52,8 +54,14 @@ fn get_key_value_from_body<'a>(key: &'a str, text: &'a str) -> Option<regex::Mat
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: Read video id from args
-    let video_id = "OLKhm1jKrVM";
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please add video id.");
+        println!("Example: ripytc dPX0_IEXVRo");
+        exit(1)
+    }
+
+    let video_id = &args[1];
     let video = Video::from_id(video_id).await;
     print!("{:?}", video);
     //TODO: get_live_chat_replay
