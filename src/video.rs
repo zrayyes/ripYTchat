5e6aac2c761447bb8a154128e6b2ac24eadc000b
Zuhair Rayyes <rayyes95@gmail.com>
2022-10-15 19:28:50 +0200

    added better error handling, missing tests

--- src/video.rs
+++ src/video.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regex::{Error, Regex};
 
 #[derive(Debug)]
 pub struct Video {
@@ -11,41 +11,55 @@ pub struct Video {
 }
 
 impl Video {
-    pub async fn from_id(video_id: &str) -> Video {
-        let video_url = format!("https://www.youtube.com/watch?v={}", &video_id);
-
-        let body = reqwest::get(&video_url)
-            .await
-            .expect("Failed to fetch video page from video ID.")
-            .text()
-            .await
-            .expect("Failed to get video page text.");
-        let continuation_key = get_key_value_from_body("continuation", &body)
+    pub async fn from_id(video_id: &str) -> Result<Video, Box<dyn std::error::Error>> {
+        let body = get_video_body(&video_id).await?;
+        let continuation_key = get_key_value_from_body("continuation", &body)?
             .unwrap()
             .as_str();
-        let api_key = get_key_value_from_body("INNERTUBE_API_KEY", &body)
+        let api_key = get_key_value_from_body("INNERTUBE_API_KEY", &body)?
             .unwrap()
             .as_str();
-        let title = get_key_value_from_body("title", &body).unwrap().as_str();
-        let channel_name = get_key_value_from_body("author", &body).unwrap().as_str();
-        let channel_id = get_key_value_from_body("channelId", &body)
+        let title = get_key_value_from_body("title", &body)?.unwrap().as_str();
+        let channel_name = get_key_value_from_body("author", &body)?.unwrap().as_str();
+        let channel_id = get_key_value_from_body("channelId", &body)?
             .unwrap()
             .as_str();
 
-        Video {
+        Ok(Video {
             id: video_id.to_string(),
             api_key: api_key.to_string(),
             first_continuation_key: continuation_key.to_string(),
             title: title.to_string(),
             channel_name: channel_name.to_string(),
             channel_id: channel_id.to_string(),
-        }
+        })
     }
 }
 
-fn get_key_value_from_body<'a>(key: &'a str, text: &'a str) -> Option<regex::Match<'a>> {
+fn get_key_value_from_body<'a>(
+    key: &'a str,
+    text: &'a str,
+) -> Result<Option<regex::Match<'a>>, String> {
     let re_str = format!(r#""{}":"(.*?)","#, key);
-    let re = Regex::new(&re_str).unwrap();
-    let caps = re.captures(text).unwrap();
-    caps.get(1)
+    let re = Regex::new(&re_str).expect(&format!("Invalid regex: {}", &re_str));
+    let caps = match re.captures(text) {
+        Some(caps) => caps,
+        None => return Err(format!("Key '{}' not found in HTML body.", key)),
+    };
+    Ok(caps.get(1))
+}
+
+async fn get_video_body(video_id: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let video_url = format!("https://www.youtube.com/watch?v={}", &video_id);
+
+    let response = reqwest::get(&video_url)
+        .await
+        .expect("Failed to fetch video page from video ID.");
+
+    let body = response
+        .text()
+        .await
+        .expect("Failed to get video page text.");
+
+    Ok(body)
 }
