aa60ea3b24a6c5f027ae85cb4b8ae28898e1abf4
Zuhair Rayyes <rayyes95@gmail.com>
2022-10-21 18:02:48 +0200

    updated models

--- src/store/models.rs
+++ src/store/models.rs
@@ -1,4 +1,25 @@
-pub struct Video;
-pub struct Message;
-pub struct Channel;
-pub struct Emote;
+pub struct Video {
+    id: String,
+    title: String,
+}
+pub struct Message {
+    content: String,
+    author: String,
+    offset: String,
+}
+pub struct Channel {
+    id: String,
+    name: String,
+}
+pub struct Emote {
+    id: String,
+    shortcut: String,
+    url: String,
+}
+
+pub struct Aggregate {
+    video: Video,
+    channel: Channel,
+    messages: Vec<Message>,
+    emotes: Vec<Emote>,
+}
